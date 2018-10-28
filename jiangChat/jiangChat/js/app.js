29aaf272411f26f1eed34d45b6ea96ca00900de1
jiangge <m15101653938@163.com>
2018-10-29 00:01:20 +0800

    公共js 编写

--- jiangChat/jiangChat/js/app.js
+++ jiangChat/jiangChat/js/app.js
@@ -1,5 +1,9 @@
 window.app{
 	/**
+	 * 后端发布的url地址
+	 */
+	serverUrl:"http://192.168.1.5:8080",
+	/**
 	 * 判断是否为空：ture-不为空；false-为空
 	 */
 	isNotNull = function(str){
@@ -11,5 +15,16 @@ window.app{
 	},
 	showToast = function(msg,type){
 		plus.nativeUI.toast(msg,{icon:"image/"+ type + ".png",verticalAlign:"center"})
+	},
+	setUserGlobalInfo = function(user){
+		//将用户信息对象转换为json字符串
+		var userInfoStr = Json.stringify(user);
+		plus.storage.setItem("userInfo",userInfoStr);
+		
+	},
+	getUserGlobaInfo = function(){
+		var userInfoStr = plus.storage.getItem("userInfo");
+		return userInfoStr;
 	}
+	
 }
