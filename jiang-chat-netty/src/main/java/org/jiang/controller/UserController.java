8c2ec8bf80a7a06a52fcd23ad5b00b3fc6e37ae8
jiangge <m15101653938@163.com>
2018-11-05 23:49:42 +0800

    Update jiang-chat-netty/src/main/java/org/jiang/controller/UserController.java

--- jiang-chat-netty/src/main/java/org/jiang/controller/UserController.java
+++ jiang-chat-netty/src/main/java/org/jiang/controller/UserController.java
@@ -72,7 +72,7 @@ public class UserController {
 	public JiangJSONResult uploadFaceBase64(@RequestBody UsersBO userBO) throws Exception{
 		// 获取前端传过来的base64字符串, 然后转换为文件对象再上传
 				String base64Data = userBO.getFaceData();
-				String userFacePath = "C:\\" + userBO.getUserId() + "userface64.png";
+				String userFacePath = "D:\\" + userBO.getUserId() + "userface64.png";
 				FileUtils.base64ToFile(userFacePath, base64Data);
 				
 				// 上传文件到fastdfs
